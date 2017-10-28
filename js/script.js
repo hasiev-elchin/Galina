b2a390c345fa8aeca1899447249317cbf649f6df
Elchin <hasiev.elchin98@gmail.com>
2017-10-28 21:27:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+var map;
+function initMap() {
+
+	var myLatLng = {lat: 46.6681861, lng: 32.6658393};
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		zoom: 15,
+		disableDefaultUI: true
+	});
+}
+
 ;(function ($) {
 	$(document).ready(function(){
 		$('.slider').slick({
@@ -34,14 +46,4 @@ winCloseBtn.addEventListener("click", function (event) {
 	feedBackPopup.classList.remove("block");
 });
 
-var map;
-function initMap() {
 
-	var myLatLng = {lat: 46.6681861, lng: 32.6658393};
-
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: myLatLng,
-		zoom: 15,
-		disableDefaultUI: true
-	});
-}
