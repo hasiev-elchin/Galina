3f64ecd418c38df6528abed7224c34dedd486798
Elchin <hasiev.elchin98@gmail.com>
2017-10-29 00:52:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,6 +34,75 @@ winCloseBtn.addEventListener("click", function (event) {
 	feedBackPopup.classList.remove("block");
 });
 
+
+var certificate1 = document.querySelector(".certificate__1");
+var certificate2 = document.querySelector(".certificate__2");
+var certificate3 = document.querySelector(".certificate__3");
+var certificate4 = document.querySelector(".certificate__4");
+var certificate5 = document.querySelector(".certificate__5");
+var certificateModal1 = document.querySelector(".certificate-modal--1");
+var certificateModal2 = document.querySelector(".certificate-modal--2");
+var certificateModal3 = document.querySelector(".certificate-modal--3");
+var certificateModal4 = document.querySelector(".certificate-modal--4");
+var certificateModal5 = document.querySelector(".certificate-modal--5");
+var certificateModalClose1 = document.querySelector(".certificate-modal__close1");
+var certificateModalClose2 = document.querySelector(".certificate-modal__close2");
+var certificateModalClose3 = document.querySelector(".certificate-modal__close3");
+var certificateModalClose4 = document.querySelector(".certificate-modal__close4");
+var certificateModalClose5 = document.querySelector(".certificate-modal__close5");
+
+certificate1.addEventListener("click", function (e) {
+	e.preventDefault();
+	certificateModal1.classList.add("block");
+});
+
+certificate2.addEventListener("click", function (e) {
+	e.preventDefault();
+	certificateModal2.classList.add("block");
+});
+
+certificate3.addEventListener("click", function (e) {
+	e.preventDefault();
+	certificateModal3.classList.add("block");
+});
+
+certificate4.addEventListener("click", function (e) {
+	e.preventDefault();
+	certificateModal4.classList.add("block");
+});
+
+certificate5.addEventListener("click", function (e) {
+	e.preventDefault();
+	certificateModal5.classList.add("block");
+});
+
+
+certificateModalClose1.addEventListener("click", function (event) {
+	event.preventDefault();
+	certificateModal1.classList.remove("block");
+});
+
+certificateModalClose2.addEventListener("click", function (event) {
+	event.preventDefault();
+	certificateModal2.classList.remove("block");
+});
+
+certificateModalClose3.addEventListener("click", function (event) {
+	event.preventDefault();
+	certificateModal3.classList.remove("block");
+});
+
+certificateModalClose4.addEventListener("click", function (event) {
+	event.preventDefault();
+	certificateModal4.classList.remove("block");
+});
+
+certificateModalClose5.addEventListener("click", function (event) {
+	event.preventDefault();
+	certificateModal5.classList.remove("block");
+});
+
+
 var map;
 function initMap() {
 
@@ -45,5 +114,3 @@ function initMap() {
 		disableDefaultUI: true
 	});
 }
-
-
